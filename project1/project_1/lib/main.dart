8a3ae471983b08aaf171dd809d6b58def3878800
gardgundersen44 <gardgundersen4@gmail.com>
2024-01-03 15:20:20 +0100

    v 1.0.1

--- project1/project_1/lib/main.dart
+++ project1/project_1/lib/main.dart
@@ -1,20 +1,66 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar:
-            AppBar(backgroundColor: Colors.red, title: Text("My First App")),
-      ),
-      theme: ThemeData.dark(),
+    return MaterialApp(home: ToDoWindow());
+  }
+}
+
+class ToDoBar extends AppBar {
+  ToDoBar({super.key})
+      : super(
+            title: const Text('To Do App',
+                style: TextStyle(color: Colors.white, fontSize: 30.0)),
+            centerTitle: true,
+            backgroundColor: Colors.green.shade200);
+}
+
+class ToDoWindow extends StatelessWidget {
+  const ToDoWindow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: ToDoBar(), body: ToDoList());
+  }
+}
+
+class ToDoList extends StatelessWidget {
+  const ToDoList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      ToDoButtonList(),
+    ]);
+  }
+}
+
+class ToDoButtonList extends StatelessWidget {
+  const ToDoButtonList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Text("To Do List"),
+        SizedBox(width: 50),
+        Row(children: <Widget>[
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(minimumSize: Size(50, 50)),
+            child: Icon(Icons.add),
+          ),
+          SizedBox(width: 10),
+          ElevatedButton(onPressed: () {}, child: Icon(Icons.delete)),
+          SizedBox(width: 10),
+          ElevatedButton(onPressed: () {}, child: Icon(Icons.edit)),
+        ]),
+      ],
     );
   }
 }
